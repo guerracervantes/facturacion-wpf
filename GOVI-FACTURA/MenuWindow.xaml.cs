e1dcb38ddea415abf17070b1308d5723924709e9
Humberto Guerra <guerracervantes@gmail.com>
2026-04-02 08:08:50 -0600

    Se Agrego Menu y Login Diseño y funcional

---
+++ GOVI-FACTURA/MenuWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GOVI_FACTURA
+{
+    /// <summary>
+    /// Lógica de interacción para MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFacturacion_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+
+            this.Close();
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
